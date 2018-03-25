79d5f73a634d9284ac4e756747750726212ef075
justin <justin.ch.goh@gmail.com>
2018-03-25 15:57:33 +0800

    18

--- 18.py
+++ 18.py
@@ -27,6 +27,9 @@ class triangle_node:
         self.row = row
         self.position = position
         self.path_history = [position] if not argv else argv[0]
+
+    def __repr__(self):
+        return "Value: " + str(self.value) + ", Row: " + str(self.row) + ", Position: " + str(self.position) + ", History: " + str(self.path_history)
         
     def merge_downwards(self, node):
         # create a merged node with new properties
@@ -34,7 +37,7 @@ class triangle_node:
             (self.value + node.value),
             (node.row),
             (node.position),
-            (self.path_history + [node.position]))
+            (self.path_history + node.path_history))
         return new_node
         
     def merge_upwards(self, node):
@@ -62,5 +65,38 @@ print(w.value, w.row, w.position, w.path_history)
 # need to convert the data into a giant triangle structure storing nodes
 # and write functions to iterate through the giant triangle while operating on nodes
 # the triangle after each operation should lose the top row and the bottom row, and the 2nd and 2nd last rows
-# should be new nodes with the maximum values and path histories stored together.
+# should be ne  nodes with the maximum values and path histories stored together.
 
+big_triangle = [[75],[95,64],[17,47,82],[18,35,87,10],[20,4,82,47,65],[19,1,23,75,3,34],[88,2,77,73,7,63,67],[99,65,4,28,6,16,70,92],[41,41,26,56,83,40,80,70,33],[41,48,72,33,47,32,37,16,94,29],[53,71,44,65,25,43,91,52,97,51,14],[70,11,33,28,77,73,17,78,39,68,17,57],[91,71,52,38,17,14,91,43,58,50,27,29,48],[63,66,4,68,89,53,67,30,73,16,69,87,40,31],[4,62,98,27,23,9,70,98,73,93,38,53,60,4,23]]
+
+# big_triangle = [[3],[7,4],[2,4,6],[8,5,9,3]]
+node_triangle = []
+for idx, row in enumerate(big_triangle):
+    print(idx, row) #stick to 0 indexing
+    node_triangle.append([])
+    for idx2, value in enumerate(row):
+        node_triangle[idx].append(triangle_node(value, idx, idx2))
+
+# print(node_triangle)
+
+# collapsing triangle together from both ends
+while len(node_triangle) > 1:
+    for i, r in enumerate(node_triangle[1]):
+        # when merging from top, the middle numbers need to replace themselves with the bigger of the above 2 nodes
+        if i == 0:
+            node_triangle[1][i] = node_triangle[0][0].merge_downwards(node_triangle[1][i])
+        elif i == len(node_triangle[1]) - 1:
+            node_triangle[1][i] = node_triangle[0][i - 1].merge_downwards(node_triangle[1][i])
+        else:
+            bigger_node = node_triangle[0][i-1].merge_downwards(node_triangle[1][i]) if node_triangle[0][i-1].value > node_triangle[0][i].value else node_triangle[0][i].merge_downwards(node_triangle[1][i])
+            node_triangle[1][i] = bigger_node
+    node_triangle.pop(0)
+    
+biggest_value = 0
+biggest_path = []
+for node in node_triangle[0]:
+    if node.value > biggest_value:
+        biggest_value = node.value
+        biggest_path = node
+
+print(biggest_path)
